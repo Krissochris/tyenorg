9b39d2724610a2ca25f22d8a353a6bc0c5178cac
saviobosco <Johnboscoomebe@yahoo.com>
2020-05-10 10:39:54 +0100

    Edited the User blog create view

--- packages/GriffonTech/Shop/src/Resources/views/user/account/blog/create.blade.php
+++ packages/GriffonTech/Shop/src/Resources/views/user/account/blog/create.blade.php
@@ -8,23 +8,23 @@
 @section('content')
     <br>
     <div class="container">
-        <div class="row">
-            <div class="col-sm-12">
+        <div class="row justify-content-center">
+            <div class="col-sm-10">
                 {!! Form::open(['route' => 'user.blog.create', 'enctype' => 'multipart/form-data']) !!}
                 <div class="card">
-                    <h5 class="card-header">Create Blog</h5>
+                    <h4 class="card-header py-xl-3 text-center">Create Blog</h4>
                     <div class="card-body">
 
                         @if(isset($blog_categories) && !$blog_categories->isEmpty())
                         <div class="form-group">
                             <label for="blog_category">Blog Category</label>
-                            {!! Form::select('blog_category_id', $blog_categories, null, ['class' => 'form-control']) !!}
+                            {!! Form::select('blog_category_id', $blog_categories, null, ['class' => 'form-control shadow-sm']) !!}
                         </div>
                         @endif
 
                         <div class="form-group">
                             <label for="title">Title</label>
-                            {!! Form::text('title', null, ['class' => 'form-control', 'placeholder' => 'Blog Title']) !!}
+                            {!! Form::text('title', null, ['class' => 'form-control shadow-sm', 'placeholder' => 'Blog Title']) !!}
                         </div>
 
                         <div class="form-group">
@@ -32,19 +32,30 @@
                             {!! Form::textarea('body', null, ['class' => 'form-control tinymce_editor', 'cols' => 30, 'rows' => 10]) !!}
                         </div>
 
-                            <div class="form-group">
-                                <label for="body"> Summary </label>
-                                {!! Form::textarea('summary', null, ['class' => 'form-control', 'rows' => 5]) !!}
+                        <div class="form-group">
+                            <label for="body"> Summary </label>
+                            {!! Form::textarea('summary', null, ['class' => 'form-control shadow-sm', 'rows' => 5]) !!}
+                        </div>
+
+                        <div class="row">
+                            <div class="col-xl-6">
+                                <div class="form-group">
+                                    <label for="customFile">Blog Image</label>
+                                    <div class="custom-file">
+                                        {!! Form::file('photo', ['class' => 'custom-file-input shadow-sm']) !!}
+                                        <label for="customFile" class="custom-file-label">Choose file</label>
+                                    </div>
+                                </div>
                             </div>
 
-                        <div class="custom-file" >
-                            {!! Form::file('photo', ['class' => 'custom-file']) !!}
+                            <div class="col-xl-6">
+                                <div class="form-group">
+                                    <label for="">Status</label>
+                                    {!! Form::select('status', ['1' => 'Published', '0' => 'UnPublish'], null, ['class' => 'form-control shadow-sm']) !!}
+                                </div>
+                            </div>
                         </div>
 
-                        <div class="form-group float-left">
-                            <label for="">Status</label>
-                            {!! Form::select('status', ['1' => 'Published', '0' => 'UnPublish'], null, ['class' => 'form-control']) !!}
-                        </div>
 
                     </div>
                     <div class="card-footer">
